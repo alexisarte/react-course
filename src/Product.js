68d44a7a73f67637cc679e6284e010426c63a30e
alexisarte <alexisrodriguezarteaga@gmail.com>
2022-08-16 16:45:35 -0300

    JSX Extension

---
+++ src/Product.js
@@ -0,0 +1,9 @@
+function Product() {
+    return <h1>Prodcut</h1>
+}
+
+export function navBar() {
+    return <h1>NavBar</h1>
+}
+
+export default Product;
--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
     <Greeting />
-    <UserCard />
-    <Product />
+    <Greeting />
+    <Greeting />
   </>
 );
