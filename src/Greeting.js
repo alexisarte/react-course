ccec5521b7c9a00058bfe5f479f6390d5c9e62a5
alexisarte <alexisrodriguezarteaga@gmail.com>
2022-08-16 18:15:04 -0300

    React Props

--- src/Greeting.js
+++ src/Greeting.js
@@ -1,7 +1,22 @@
-export function Greeting(props) {
-  return <h1>Componente de React</h1>;
+export function Greeting({ tittle, name = 'user' }) {
+  return (
+    <h1>
+      {tittle}, dice {name}
+    </h1>
+  );
 }
 
-export function UserCard() {
-  return <h1>User Card</h1>;
+export function UserCard({ name, amount, married, address, greet}) {
+  console.log(name, amount, married, address, greet);
+  return (
+    <div>
+      <h1>{name}</h1>
+      <p>💲{amount}</p>
+      <p>{married ? 'Married' : 'Single'}</p>
+      <ul>
+        <li>City: {address.city}</li>
+        <li>State: {address.state}</li>
+      </ul>
+    </div>
+  );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {Greeting, UserCard} from './Greeting';
-import Product, {navBar} from './Product';
+import { Greeting, UserCard } from './Greeting';
+import Product, { navBar } from './Product';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <>
-    <Greeting />
-    <Greeting />
-    <Greeting />
+    <UserCard
+      name="Ryan Ray"
+      amount={3000}
+      married={true}
+      points={[99, 35, 254.3]}
+      address={{ street: '123 Main St', city: 'Anytown', state: 'CA' }}
+      greet={function () { alert('Hello'); } }
+    />
+
+    <UserCard 
+      name="John Doe" 
+      amount={1000} 
+      married={false}
+      points={[99, 35, 254.3]}
+      address={{ 
+        street: '123 Main St', 
+        city: 'Anytown'
+      }}  
+    />
   </>
 );
