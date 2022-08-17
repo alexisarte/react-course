3e8fabefdcae5c96502354f2e58024636fd4aacf
alexisarte <alexisrodriguezarteaga@gmail.com>
2022-08-17 13:18:58 -0300

    Styles in React

--- src/index.js
+++ src/index.js
@@ -3,34 +3,12 @@ import ReactDOM from 'react-dom/client';
 import { Greeting, UserCard } from './Greeting';
 import Product, { navBar } from './Product';
 import { Button } from './Button';
+import { TaskCard } from './Task';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <>
-    <Button text="Click me" name="John" />
-    <Button text="Pay" />
-    <Button text='' />
-    {/* <UserCard
-      name="Ryan Ray"
-      amount={3000}
-      married={true}
-      points={[99, 35, 254.3]}
-      address={{ street: '123 Main St', city: 'Anytown', state: 'CA' }}
-      greet={function () {
-        alert('Hello');
-      }}
-    />
-
-    <UserCard
-      name="John Doe"
-      amount={1000}
-      married={false}
-      points={[99, 35, 254.3]}
-      address={{
-        street: '123 Main St',
-        city: 'Anytown',
-      }}
-    /> */}
+    <TaskCard ready={true} />
   </>
 );
