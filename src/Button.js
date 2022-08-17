46f012380c644ee88a322c74025eab22b37a8de4
alexis <alexisrodriguezarteaga@gmail.com>
2022-08-17 14:18:51 -0300

    Event handlers

--- src/Button.js
+++ src/Button.js
@@ -2,11 +2,12 @@ import PropTypes from 'prop-types';
 
 
 export function Button({ text, name = '' }) {
-  console.log(text, name);
   if (!text) {
     console.error('El texto es requerido');
   }
-  return <button>
+  return <button onClick={function () {
+    console.log(text, name);
+  }}>
     {text} - {name}
   </button>;
 }
--- src/index.js
+++ src/index.js
@@ -8,9 +8,25 @@ import { Saludar } from './Saludar';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
+const handleChange = e => {
+  console.log(e.target.value + '...');
+  console.log('Escribiendo en el input');
+};
+
 root.render(
   <>
     <TaskCard ready={true} />
     <Saludar />
+    <Button text="Hola mundo" />
+
+    <input id="hola" onDoubleClick={() => console.log('doble click')} />
+
+    <form onSubmit={(e) => {
+      e.preventDefault();
+      alert('submit');
+    }}>
+      <h1>Registro de usuario</h1>
+      <button>Send</button>
+    </form>
   </>
 );
