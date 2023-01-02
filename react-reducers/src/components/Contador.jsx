ab5de98835c5bec24061e71657b85f3b3713df60
alexis <alexisrodriguezarteaga@gmail.com>
2023-01-02 19:51:07 -0300

    Hook useReducer

--- react-reducers/src/components/Contador.jsx
+++ react-reducers/src/components/Contador.jsx
@@ -2,12 +2,26 @@ import { useState, useReducer } from 'react';
 
 const initialState = { count: 0 };
 
+const TYPES = {
+  INCREMENT: 'INCREMENT',
+  INCREMENT_5: 'INCREMENT_5',
+  DECREMENT: 'DECREMENT',
+  DECREMENT_5: 'DECREMENT_5',
+  RESET: 'RESET',
+};
+
 function reducer(state, action) {
   switch (action.type) {
-    case 'INCREMENT':
+    case TYPES.INCREMENT:
       return { count: state.count + 1 };
-    case 'DECREMENT':
+    case TYPES.DECREMENT:
       return { count: state.count - 1 };
+    case TYPES.RESET:
+      return initialState;
+    case TYPES.INCREMENT_5:
+      return { count: state.count + action.payload };
+    case TYPES.DECREMENT_5:
+      return { count: state.count - action.payload };
     default:
       return state;
   }
@@ -18,10 +32,16 @@ const Contador = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
   // const sumar = () => setCount(count + 1);
-  const sumar = () => dispatch({ type: 'INCREMENT' });
+  const sumar = () => dispatch({ type: TYPES.INCREMENT });
+
+  const sumar5 = () => dispatch({ type: TYPES.INCREMENT_5, payload: 5 });
 
   // const restar = () => setCount(count - 1);
-  const restar = () => dispatch({ type: 'DECREMENT' });
+  const restar = () => dispatch({ type: TYPES.DECREMENT });
+
+  const restar5 = () => dispatch({ type: TYPES.DECREMENT_5, payload: 5 });
+
+  const reset = () => dispatch({ type: TYPES.RESET });
 
   return (
     <div style={{ textAlign: 'center' }}>
@@ -29,6 +49,9 @@ const Contador = () => {
       <nav>
         <button onClick={sumar}>+</button>
         <button onClick={restar}>-</button>
+        <button onClick={reset}>Reset</button>
+        <button onClick={sumar5}>+5</button>
+        <button onClick={restar5}>-5</button>
       </nav>
       <h3>{state.count}</h3>
     </div>
