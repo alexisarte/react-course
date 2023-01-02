90b49697ffa572801e0aafa5730fb92f00c3bf09
alexis <alexisrodriguezarteaga@gmail.com>
2023-01-02 19:19:10 -0300

    Reducers Introducción

---
+++ react-reducers/src/App.jsx
@@ -0,0 +1,13 @@
+// import './App.css'
+import Contador from './components/Contador'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>useReducer</h1>
+      <Contador />
+    </div>
+  )
+}
+
+export default App
--- src/Product.js
+++
@@ -1,9 +0,0 @@
-function Product() {
-    return <h1>Prodcut</h1>
-}
-
-export function navBar() {
-    return <h1>NavBar</h1>
-}
-
-export default Product;
