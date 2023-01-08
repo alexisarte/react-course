3bb099014ac345173ca40181636537a578bfb1aa
alexis <alexisrodriguezarteaga@gmail.com>
2023-01-08 17:12:09 -0300

    Memorización (useCallback)

--- 04-react-memorizacion/src/components/Contador.jsx
+++ 04-react-memorizacion/src/components/Contador.jsx
@@ -1,23 +1,32 @@
-import { useState } from "react";
-import ContadorHijo from "./ContadorHijo";
+import { useCallback, useState } from 'react';
+import ContadorHijo from './ContadorHijo';
 
 const Contador = () => {
   const [contador, setContador] = useState(0);
+  const [input, setInput] = useState('');
+
+  // const sumar = () => setContador(contador + 1);
+  const sumar = useCallback(() => setContador(contador + 1), [contador]);
   
-  const sumar = () => setContador(contador + 1);
-  const restar = () => setContador(contador - 1);
+  // const restar = () => setContador(contador - 1);
+  const restar = useCallback(() => setContador(contador - 1), [contador]);
+
+  const handleInput = (e) => {
+    setInput(e.target.value);
+  };
 
   return (
-    <div style={{textAlign: 'center'}}>
+    <div style={{ textAlign: 'center' }}>
       <h2>Contador</h2>
       <nav>
         <button onClick={sumar}>+</button>
         <button onClick={restar}>-</button>
       </nav>
       <h3>{contador}</h3>
-      <ContadorHijo />
+      <input type="text" onChange={handleInput} value={input} />
+      <ContadorHijo contador={contador} sumar={sumar} restar={restar} />
     </div>
-  )
-}
+  );
+};
 
-export default Contador
+export default Contador;
--- 04-react-memorizacion/src/components/ContadorHijo.jsx
+++ 04-react-memorizacion/src/components/ContadorHijo.jsx
@@ -1,11 +1,16 @@
 import { memo } from 'react';
 
-const ContadorHijo = () => {
+const ContadorHijo = ({contador, sumar, restar}) => {
   console.log('  Me volví a generar :(  ');
 
   return (
     <div style={{ border: 'thin solid #000', margin: '1rem', padding: '1rem' }}>
       <h2>Contador Hijo</h2>
+      <nav>
+        <button onClick={sumar}>+</button>
+        <button onClick={restar}>-</button>
+      </nav>
+      <h3>{contador}</h3>
     </div>
   );
 };
